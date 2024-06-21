8607e3dd876f00a7df8a3691d52e768e30b704c9
TurboCaveMan <cameronturner55@gmail.com>
2024-06-21 13:10:36 -0700

    images added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,13 @@ const mockImages = mockUrls.map((url, index) => ({
 export default function HomePage() {
   return (
     <main className="">
+      <div className="flex flex-wrap">{
+        mockImages.map((image) => (
+          <div key={image.id} className="w-1/2 p-4">
+            <img src={image.url} alt="image" />
+          </div>
+        ))
+    }</div>
       Hello (Gallery in Progress)
     </main>
   );
