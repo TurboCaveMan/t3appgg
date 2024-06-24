7948e78cb2827794fcd5faac6f21b453d830c659
TurboCaveMan <cameronturner55@gmail.com>
2024-06-24 12:53:31 -0700

    signin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,38 @@
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic ="force-dynamic";
 
-export default async function HomePage() {
-
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy:(model, { desc }) => desc(model.id),
   });
+  return(
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={image.id + "-" + index} className="flex w-48 flex-col">
+          <img src={image.url} alt="image" />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+
+export default async function HomePage() {
+
+
 
 
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col">
-            <img src={image.url} alt="image" />
-            <div>{image.name}</div>
-          </div>
-        ))
-    }</div>
-      Hello (Gallery in Progress)
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
